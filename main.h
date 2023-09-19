cad146767889c5c98eb13c900c58ee07d1cb8b47
Elshater <elshater.ehab@gmail.com>
2023-09-19 16:43:57 -0700

    adding main.h

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef PRINTF_MAIN_H
+#define PRINTFO_MAIN_H
+
+
+#define NULL_STRING "(null)"
+
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int _puts(char *s);
+
+int _putchar(int c);
+
+int print_digit(long n, int base);
+
+int print_format(char specifier, va_list ap);
+
+int print_percent(void);
+
+int _printf(char *format, ...);
+
+
+
+#endif
+
